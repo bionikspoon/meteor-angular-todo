bcb80ffd106dfed1c3a2d6201fe64333038473b6
Manu Phatak <bionikspoon@gmail.com>
2016-04-18 00:07:04 -0500

    Setup Tasks collection

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -1,20 +1,22 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import { Tasks } from '../../api/tasks';
 import './todosList.html';
 
 export default angular
   .module('todosList', [ angularMeteor ])
   .component('todosList', {
     templateUrl: 'imports/components/todosList/todosList.html',
-    controller: TodosListController,
+    controller: [ '$scope', TodosListController ],
   });
 
-function TodosListController() {
+/* @ngInject */
+function TodosListController($scope) {
   const $ctrl = this;
 
-  $ctrl.tasks = [
-    { text: 'The moon has conclusion, but not everyone emerges it.' },
-    { text: 'One must acquire the spirit in order to love the cow of meaningless hypnosis.' },
-    { text: 'Stop to remember and lure.' },
-  ];
+  $scope.viewModel($ctrl);
+
+  $ctrl.helpers({
+    tasks() {return Tasks.find({});},
+  });
 }
