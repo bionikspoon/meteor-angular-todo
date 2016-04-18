0e282c36a99f34b3869ff6d9deedf3157b031d87
Manu Phatak <bionikspoon@gmail.com>
2016-04-17 23:51:34 -0500

    add eslint, fix lint issues

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
-import todosList from '../imports/components/todosList/todosList'
+import todosList from '../imports/components/todosList/todosList';
 angular.module('app', [ angularMeteor, todosList.name ]);
--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -1,13 +1,13 @@
-import angular from 'angular'
-import angularMeteor from 'angular-meteor'
-import template from './todosList.html';
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import './todosList.html';
 
 export default angular
   .module('todosList', [ angularMeteor ])
   .component('todosList', {
     templateUrl: 'imports/components/todosList/todosList.html',
     controller: TodosListController,
-  })
+  });
 
 function TodosListController() {
   const $ctrl = this;
@@ -16,5 +16,5 @@ function TodosListController() {
     { text: 'The moon has conclusion, but not everyone emerges it.' },
     { text: 'One must acquire the spirit in order to love the cow of meaningless hypnosis.' },
     { text: 'Stop to remember and lure.' },
-  ]
+  ];
 }
