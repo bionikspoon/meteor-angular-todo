7e9d2fa0139b7e892265fae26fd016915cc470dc
Manu Phatak <bionikspoon@gmail.com>
2016-04-18 03:27:18 -0500

    setup android app

--- client/main.js
+++ client/main.js
@@ -1,4 +1,17 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import todosList from '../imports/components/todosList/todosList';
+
 angular.module('app', [ angularMeteor, todosList.name ]);
+
+function onReady() {
+  angular.bootstrap(document, [ 'app' ]);
+}
+
+if (Meteor.isCordova) {
+  angular.element(document).on('deviceready', onReady);
+}
+else {
+  angular.element(document).ready(onReady);
+}
+
