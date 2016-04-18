7596897ee5e8beddafb76b0a7723c005d9728f2e
Manu Phatak <bionikspoon@gmail.com>
2016-04-17 23:40:36 -0500

    Initial Commit

---
+++ client/main.js
@@ -0,0 +1,4 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import todosList from '../imports/components/todosList/todosList'
+angular.module('app', [ angularMeteor, todosList.name ]);
---
+++ imports/components/todosList/todosList.js
@@ -0,0 +1,20 @@
+import angular from 'angular'
+import angularMeteor from 'angular-meteor'
+import template from './todosList.html';
+
+export default angular
+  .module('todosList', [ angularMeteor ])
+  .component('todosList', {
+    templateUrl: 'imports/components/todosList/todosList.html',
+    controller: TodosListController,
+  })
+
+function TodosListController() {
+  const $ctrl = this;
+
+  $ctrl.tasks = [
+    { text: 'The moon has conclusion, but not everyone emerges it.' },
+    { text: 'One must acquire the spirit in order to love the cow of meaningless hypnosis.' },
+    { text: 'Stop to remember and lure.' },
+  ]
+}
